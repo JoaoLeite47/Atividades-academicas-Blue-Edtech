39cf653062ecad3e7b6a5d1a7602d7eaa79c1870
JoaoLeite47 <StalkerJJ@outlook.com>
2022-03-14 20:31:54 -0300

    commit

---
+++ Projetos_modulo l/JoKenPo.js
@@ -0,0 +1,89 @@
+//comando para prompt!
+const prompt = require('prompt-sync')();
+//variaveis inclusas no jogo
+do {
+    let rodadas = 0;
+    let win = 0;
+    let fail = 0;
+    let num = 0;
+    let jogadas_player = [];
+    let jogadas_pc = [];
+    let jogadas = [0, 1, 2]; // 0 = pedra, 1 = papel, 2 = tesoura!
+    let rounds = +prompt('Quantas rodadas? ');
+    //base de funcionamento do jogo com looping de rodadas
+    do {
+        rodadas++;
+        //funcionamento da IA
+        let random_number = Math.floor(Math.random() * 3);
+        let jogadas_config = +prompt('(0)pedra (1)papel ou (2)tesoura? ');
+        jogadas_player.push(jogadas_config);
+        let jogadas_config2 = jogadas[random_number];
+        jogadas_pc.push(jogadas_config2);
+        console.log(`o computador escolheu: ${jogadas_pc}`);
+        //funcionamento da quebra de código!
+        if (isNaN(jogadas_config)) {
+            // Quebra de codigo em caso de NaN
+            console.log('por favor!, apenas (1) ou (2) ou (3)');
+            break;
+        } else {
+            console.log('\nVamos jogar!!');
+        }
+        //funcionamento do score!
+        for (var final_player of jogadas_player) {
+        }
+        for (var final_pc of jogadas_pc) {
+        }
+        //funções para vitórias, derrotas e empates!
+        function winn() {
+            win++;
+            console.log('uma vitória!');
+        }
+        function faill() {
+            fail++;
+            console.log('uma derrota...');
+        }
+        function numm() {
+            num++;
+            console.log('um empate!');
+        }
+        //condicionais de vitórias, derrotas e empates!
+        if (final_player == 0) {
+            if (final_pc == 0) {
+                numm();
+            } else if (final_pc == 1) {
+                winn();
+            } else if (final_pc == 2) {
+                faill();
+            }
+        }
+        if (final_player == 1) {
+            if (final_pc == 0) {
+                winn();
+            } else if (final_pc == 1) {
+                numm();
+            } else if (final_pc == 2) {
+                faill();
+            }
+        }
+        if (final_player == 2) {
+            if (final_pc == 0) {
+                faill();
+            } else if (final_pc == 1) {
+                winn();
+            } else if (final_pc == 2) {
+                numm();
+            }
+        }
+    } while (rodadas < rounds);
+    console.log(
+        `parabéns! Você obteve: ${win} vitórias, ${fail} derrotas e ${num} empates!`,
+    );
+    //lopping para rejogar!
+    var continuar = prompt('deseja continuar?(sim ou nao)! ');
+    if (continuar == 'sim') {
+        continue;
+    } else if (continuar == 'nao') {
+        console.log('End Game!');
+        break;
+    }
+} while (continuar == 'sim');
---
+++ Projetos_modulo l/Jornada_Do_Heroi.js
@@ -0,0 +1,62 @@
+let prompt = require('prompt-sync')();
+//comando para prompt!
+console.log(
+    '\nOlá criminoso! bem vindo ao RioCity! aqui é onde o crime manda! e chegou a hora de escrever a sua história!\n',
+);
+console.log(
+    '\nMas...para isso você deve provar que é um bom homem de negócios!! Hehehe, preparado?...Vamos lá!\n',
+);
+//perguntas iniciais!
+//looping inicial do questionário em number!
+while (true) {
+    let pergunta0 = +prompt(
+        '\nUm policial aparece fazendo ronda, você negocia com ele para que lhe deixe em paz?(sim(1) ou não(0)?)',
+    );
+    let pergunta1 = +prompt(
+        '\nVocê percebe que um parceiro seu está roubando parte dos seus lucros..e você o faz pagar caro!(sim(1) ou não(0)?)',
+    );
+    let pergunta2 = +prompt(
+        '\nVocê acaba de receber uma proposta para constrabandear drogas mais pesadas por 25% dos lucros! você aceita ou renegocia?(sim(1) ou não(0)?)',
+    );
+    let pergunta3 = +prompt(
+        '\nVocê acaba descobrindo a traição do seu conjuge com seu rival!! e você não aceita perdão! (sim(1) ou não(0)?)',
+    );
+    let pergunta4 = +prompt(
+        '\nPor fim! Você fica cara a cara com o maior traficante da RioCity!..você faz ele conhecer o criador?(sim(1) ou não(0)?)',
+    );
+    let perguntas = [pergunta0, pergunta1, pergunta2, pergunta3, pergunta4];
+    //array para exclusão de "não respostas"
+    let calculo = pergunta0 + pergunta1 + pergunta2 + pergunta3 + pergunta4;
+    //calculo para resultado do questionário
+    if (perguntas === NaN) {
+        console.log(
+            '\nApenas respostas como (1) ou (0)! Recomece sua história!',
+        );
+        continue;
+    } else if (
+        (pergunta0 || pergunta1 || pergunta2 || pergunta3 || pergunta4) !== 1 ||
+        0
+    ) {
+        console.log(
+            '\nApenas respostas como (1) ou (0)! Recomece sua história!',
+        );
+        continue;
+    } else {
+        if (calculo === 5) {
+            console.log('Você..é..INEVITÁVEL! Vitória total!');
+        } else if (calculo === 4) {
+            console.log('Você chegou perto da conquista total!! parabéns!!');
+        } else if (calculo === 3) {
+            console.log(
+                'Você conquistou muitos objetivos!! mas ainda há muito a aprender!!',
+            );
+        } else if (calculo === 2 || 1) {
+            console.log(
+                ' Até no crime existem pessoas que não foram feitas pra isso!! mais sorte na próxima!',
+            );
+        } else {
+            console.log(' Você perdeu miseravelmente! Que vergonha!');
+        }
+        break;
+    }
+}
