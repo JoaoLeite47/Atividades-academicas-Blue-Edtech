3caef904435b8e43a8266ccdafcbd3a7a153dab0
JoaoLeite47 <StalkerJJ@outlook.com>
2022-03-23 10:33:31 -0300

    Projeto módulo l / Game Interativo

--- Projetos_modulo l/Projeto_Final.js
+++ Projetos_modulo l/Projeto_Final.js
@@ -21,10 +21,10 @@ do {
     console.log('Olá viajante! seja bem vindo ao D.E.V.I.L! ');
     const outlander = prompt('Digite seu nome viajante! ');
     console.log(
-        `Olá ${outlander}! você está prestes a ser inserido no incrível mundo de D.E.V.I.L!`,
+        `\nOlá ${outlander}! você está prestes a ser inserido no incrível mundo de D.E.V.I.L!`,
     );
     console.log(
-        'Suas decisões terão impacto sobre sua vida, ganho de poder e habilidades especiais, jogue com cuidado!',
+        '\nSuas decisões terão impacto sobre sua vida, ganho de poder e habilidades especiais, jogue com cuidado!',
     );
     console.log(`Nivel de vida em: ${variaveis_iniciais.saude}`);
     console.log(`Nivel de poder em: ${variaveis_iniciais.poder} `);
@@ -32,7 +32,7 @@ do {
     // NIVEL 1
     lopping_nivel1: do {
         const player_pc = [];
-        player_random = Math.floor(Math.random() * 3)
+        player_random = Math.floor(Math.random() * 3);
         player_pc.push(player_random);
 
         console.log('NIVEL 1!');
@@ -44,7 +44,7 @@ do {
         );
 
         const decisao1 = prompt(
-            'Deseja abrir o baú?(sim ou nao) ',
+            '\nDeseja abrir o baú?(sim ou nao) ',
         ).toLowerCase();
 
         if (decisao1 === 'sim') {
@@ -59,29 +59,31 @@ do {
             `Ao adentrar mais fundo o primeiro nível,${outlander} encontra quem virá a ser seu primeiro inimigo!`,
         );
         const combate1 = prompt(
-            `Você ataca primeiro?(sim ou nao) `,
+            `\nVocê ataca primeiro?(sim ou nao) `,
         ).toLowerCase();
 
         if (combate1 == 'sim') {
-            if (variaveis_iniciais.player_pc == 1) {
+            if (player_pc == 0) {
+                console.log('Sorte de principiante...');
+            } else if (player_pc == 1) {
                 console.log('Você o derrotou! sem perder vida!');
-            } else if (variaveis_iniciais.player_pc == 2) {
+            } else if (player_pc == 2) {
                 console.log('Você foi atingido! Mas consegue derrota-lo!');
                 variaveis_iniciais.saude_function;
                 console.log(`Sua saúde agora é: ${variaveis_iniciais.saude}`);
             }
         } else if (combate1 == 'nao') {
-            if (player_pc == 1) {
+            if (player_pc == 0) {
                 console.log(
                     'Você se defendeu! E o derrotou com apénas 1 golpe!',
                 );
                 variaveis_iniciais.poder_function();
                 console.log(`Seu poder agora é ${variaveis_iniciais.poder}`);
-            } else if (player_pc == 2) {
+            } else if (player_pc == 1) {
                 console.log('Você foi atingido! Mas consegue derrota-lo!');
                 variaveis_iniciais.saude_function();
                 console.log(`Sua saúde agora é: ${variaveis_iniciais.saude}`);
-            } else if (player_pc == 3) {
+            } else if (player_pc == 2) {
                 console.log('Você recebeu um golpe crítico!');
                 variaveis_iniciais.saude_function();
                 variaveis_iniciais.saude_function();
@@ -89,8 +91,8 @@ do {
                 console.log(`Sua saúde agora é ${variaveis_iniciais.saude}`);
             }
         }
-        if (variaveis_iniciais.saude == 0) {
-            console.log('Dead End!');
+        if (variaveis_iniciais.saude <= 0) {
+            console.log('Dead End! Terá que recomeçar!');
             continue lopping_nivel1;
         } else {
             console.log('Você passou de nível!');
@@ -100,7 +102,7 @@ do {
     //NIVEL 2
     lopping_nivel2: do {
         const player_pc = [];
-        player_random = Math.floor(Math.random() * 3)
+        player_random = Math.floor(Math.random() * 3);
         player_pc.push(player_random);
 
         console.log('NIVEL 2!');
@@ -109,7 +111,7 @@ do {
             'Sua experiência cresce cada vez mais e agora novos desafios surgirão!',
         );
         const decisao2 = prompt(
-            `${outlander} precisará salvar um cachorrinho triste, você salvará?(sim ou nao) `,
+            `\n${outlander} precisará salvar um cachorrinho triste, você salvará?(sim ou nao) `,
         ).toLowerCase();
         if (decisao2 == 'sim') {
             console.log(
@@ -128,22 +130,22 @@ do {
             `${outlander} acaba de encontrar um Súcubos! o Demônio comandante do segundo nível do inferno!`,
         );
         const combate2 = prompt(
-            `${outlander} decide por atacar o demônio!(sim ou nao) `,
+            `\n${outlander} decide por atacar o demônio!(sim ou nao) `,
         ).toLowerCase();
         if (combate2 == 'sim') {
-            if (player_pc == 1) {
+            if (player_pc == 0) {
                 console.log('Você deu um ataque crítico!');
                 variaveis_iniciais.super_poder_funtion();
                 console.log(
                     `Seu super poder agora é ${variaveis_iniciais.super_poder}`,
                 );
-            } else if (player_pc == 2) {
+            } else if (player_pc == 1) {
                 console.log(
                     'Você tomou um golpe...mas sua missão está cumprida!',
                 );
                 variaveis_iniciais.saude_function();
                 console.log(`Sua saúde agora é ${variaveis_iniciais.saude}`);
-            } else if (player_pc == 3) {
+            } else if (player_pc == 2) {
                 console.log('O Súcubus é um inimígo poderoso!');
                 variaveis_iniciais.saude_function();
                 variaveis_iniciais.saude_function();
@@ -156,8 +158,8 @@ do {
             );
             console.log('Ele decide por te deixar passar!');
         }
-        if (variaveis_iniciais.saude == 0) {
-            console.log('Dead End!');
+        if (variaveis_iniciais.saude <= 0) {
+            console.log('Dead End! Terá que recomeçar!');
             continue lopping_nivel2;
         } else {
             console.log('Você passou de nível!');
@@ -167,7 +169,7 @@ do {
     // //NIVEL 3
     looping_nivel3: do {
         const player_pc = [];
-        player_random = Math.floor(Math.random() * 3)
+        player_random = Math.floor(Math.random() * 3);
         player_pc.push(player_random);
 
         console.log('NIVEL 3!');
@@ -179,7 +181,7 @@ do {
         );
         if (variaveis_iniciais.super_poder == '1') {
             const bonus = prompt(
-                `${outlander} tem 1 ponto de Super poder! Deseja trocar por 1 ponto de Saúde?(sim ou nao)`,
+                `\n${outlander} tem 1 ponto de Super poder! Deseja trocar por 1 ponto de Saúde?(sim ou nao)`,
             ).toLowerCase();
             if (bonus == 'sim') {
                 variaveis_iniciais.saude++;
@@ -190,7 +192,7 @@ do {
             `${outlander} avança para as profundesas do inferno! Onde encontra velhos conhecidos!`,
         );
         const decisao3 = prompt(
-            `Velhos amigos e familiáres vem ao seu encontro! ${outlander} falará com eles?(sim ou nao) `,
+            `\nVelhos amigos e familiáres vem ao seu encontro! ${outlander} falará com eles?(sim ou nao) `,
         ).toLowerCase();
         if (decisao3 == 'sim') {
             console.log(
@@ -208,7 +210,7 @@ do {
             );
         }
         const combate3 = prompt(
-            `Sua última batalha se inicia! ${outlander} atacará primeiro?(sim ou nao) `,
+            `\nSua última batalha se inicia! ${outlander} atacará primeiro?(sim ou nao) `,
         ).toLowerCase();
         if (combate3 == 'sim') {
             console.log(
@@ -245,8 +247,8 @@ do {
                 );
             }
         }
-        if (variaveis_iniciais.saude == 0) {
-            console.log('Dead End!');
+        if (variaveis_iniciais.saude <= 0) {
+            console.log('Dead End! Terá que recomeçar!');
             continue looping_nivel3;
         } else {
             console.log('VITÓRIA!!!!');
@@ -255,5 +257,5 @@ do {
     } while (true);
     console.log(`PARABÉNS ${outlander}! Você chegou ao final!`);
     //restart
-    var restart = prompt('Deseja recomeçar?(sim ou nao) ');
+    var restart = prompt('\nDeseja recomeçar?(sim ou nao) ');
 } while (restart == 'sim');
